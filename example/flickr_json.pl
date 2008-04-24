659fdce1ce3aabbb0990a6ad231f8f8908ed086b
Yusuke Wada <yusuke@kamawada.com>
2008-04-24 12:26:52 +0000

    flickr で json フォーマットを扱う例

---
+++ example/flickr_json.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use WebService::Simple;
+use Data::Dumper;
+
+my $flickr = WebService::Simple->new(
+    base_url        => "http://api.flickr.com/services/rest/",
+    response_parser => 'JSON',
+    params          => { api_key => $ARGV[0], format => "json" }
+);
+
+my $response =
+  $flickr->get( { method => "flickr.photos.search", text => "cat" } );
+${ $response->content_ref } =~ s/jsonFlickrApi\((.+)\)/$1/;
+print Dumper $response->parse_response;
+
